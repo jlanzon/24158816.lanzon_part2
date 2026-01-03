681a75ad4293b93ed9eee995f77e35051550ebf7
jlanzon <joseph.lanzon@kryptokloud.com>
2026-01-03 23:06:46 +0000

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import javax.swing.SwingUtilities;
+import view.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+}
---
+++ src/view/MainFrame.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        setTitle("UNI");
+        setSize(1000, 650);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
